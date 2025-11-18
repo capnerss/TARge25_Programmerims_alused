31467c6534b73cfbb01d85b589f8f6969afa09be
opilane <capners29@gmail.com>
2025-11-18 14:11:12 +0200

    add koolis  to project

---
+++ 04_matemaatilised_avaldused/ex01/koolis.py
@@ -0,0 +1,20 @@
+#Python koolis
+
+def hello_world():
+    name = input("Sinu nimi")
+    print(f"Hello {name}")
+
+def calculate_in_python():
+    print(3 + 8 / (4 - 2) * 4)
+    #print(3 + 8 / 4 - 2 * 4) works same as in normal math
+
+def random_text_print():
+    random_text1 = "kill-koll kill-koll "
+    random_text2 = " killadi-koll "
+    print((random_text1 + random_text2) * 2 + random_text1 * 2)
+
+def song_text():
+    pass
+
+hello_world()
+random_text_print()
